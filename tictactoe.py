d175e9e87fd9364a8bc69ce145992655fa423f4d
Jill Wu <gena168@gmail.com>
2020-04-18 14:41:53 -0700

    tic tac toe complete

--- tictactoe.py
+++ tictactoe.py
@@ -7,18 +7,19 @@ class TicTacToe:
 		self.empty = ' '
 		self.board = [[self.empty for _ in range(self.game_size)] for _ in range(self.game_size)]
 		self.game_over = False
+		self.winner = None
 
 	def display_board(self):
 		row_divide = "---------"
-		for y in range(self.game_size):
+		for x in range(self.game_size):
 			curr_row = ""
-			for x in range(self.game_size):
-				if x == 0 or x == 1:
+			for y in range(self.game_size):
+				if y == 0 or y == 1:
 					curr_row = curr_row + self.board[x][y] + " | "
 				else:
 					curr_row = curr_row + self.board[x][y]
 			print(curr_row)
-			if y == 0 or y == 1:
+			if x == 0 or x == 1:
 				print(row_divide)
 
 	def update_board(self, player, location):
@@ -32,20 +33,71 @@ class TicTacToe:
 			print("invalid location, please enter another location")
 
 	def check_win(self):
-		pass
+		# check if any rows are completed
+		for x in range(self.game_size):
+			if self.game_over == False:
+				player_spot = self.board[x][0]
+				if player_spot != self.empty:
+					for y in range(1, self.game_size):
+						if player_spot != self.board[x][y]:
+							break
+						elif y == self.game_size - 1:
+							self.winner = player_spot
+							self.game_over = True
+
+		# check if any columns are completed
+		for y in range(self.game_size):
+			if self.game_over == False:
+				player_spot = self.board[0][y]
+				if player_spot != self.empty:
+					for x in range(1, self.game_size):
+						if player_spot != self.board[x][y]:
+							break
+						elif x == self.game_size - 1:
+							self.winner = player_spot
+							self.game_over = True
+
+		# check diagonal top left to bottom right (0,0) (1,1) (2,2)
+		if self.game_over == False:
+			player_spot = self.board[0][0]
+			if player_spot != self.empty:
+				for x in range(1, self.game_size):
+					if player_spot != self.board[x][x]:
+						break
+					elif x == self.game_size - 1:
+						self.winner = player_spot
+						self.game_over = True
+
+		# check diagonal top right to bottom left (2,0) (1,1) (0,2)
+		if self.game_over == False:
+			player_spot = self.board[2][0]
+			if player_spot != self.empty:
+				for x in range(1, self.game_size):
+					if player_spot != self.board[2-x][x]:
+						break
+					elif x == self.game_size - 1:
+						self.winner = player_spot
+						self.game_over = True
+
+
+
 
 	def player_move(self):
 		x = input("Please choose an x coordinate: ")
 		y = input("Please choose a y coordinate: ")
 		self.update_board(self.player, (int(x), int(y)))
+		if self.player == self.player_0:
+			self.player = self.player_1
+		else:
+			self.player = self.player_0
 
 	def play(self):
-		print("testing")
-		# self.update_board(self.player_0, (0, 1))
-		# self.update_board(self.player_0, (0, 1))
-		self.player_move()
-		print(self.board)
-		self.display_board()
+		while not self.game_over:
+			self.player_move()
+			self.display_board()
+			self.check_win()
+
+		print("GAME OVER, " + self.winner + " HAS WON")
 
 
 game = TicTacToe()
